c4ba648cf302253ecaa96be43d039363804d9bde
Carsten Bauer <crstnbr@gmail.com>
2019-11-25 20:37:43 +0100

    Files generated by PkgTemplates

---
+++ src/WorkshopInstaller.jl
@@ -0,0 +1,5 @@
+module WorkshopInstaller
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using WorkshopInstaller
+using Test
+
+@testset "WorkshopInstaller.jl" begin
+    # Write your own tests here.
+end
