7c1e3d7baceab575a03cc2d943aad3afff44b787
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-26 23:57:50 +0000

    build based on c50d012

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Introduction-1","page":"Introduction","title":"Introduction","text":"","category":"section"},{"location":"#","page":"Introduction","title":"Introduction","text":"WorkshopWizard.jl is awesome.","category":"page"}]
+}
